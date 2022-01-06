929faf9297f227cf679b23dd05935d2e469af657
zous71628 <zous71628@gmail.com>
2022-01-06 09:59:09 +0800

    feat(app):add cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,10 +21,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/',cors(), indexRouter);
+app.use('/users', cors(),usersRouter);
 // app.use('/posts', postsRouter);
 
+app.use(
+  cors({
+    origin: ['https://final-dbapi-1101.herokuapp.com/']
+  })
+);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
